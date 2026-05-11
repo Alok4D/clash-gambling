11434299fb45ba42c1f0c45ecd594c9a1616bff5
alok4d <alokroy602701@gmail.com>
2026-05-11 12:28:16 +0600

    feat: add DashboardHeader component with user profile information and navigation

--- components/dashboard_component/DashboardHeader.tsx
+++ components/dashboard_component/DashboardHeader.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 export function DashboardHeader() {
@@ -7,8 +8,8 @@ export function DashboardHeader() {
     <div className="w-full border-b border-[#00FF88]/20 py-4 px-6 z-40 bg-[#181818]">
       <div className="w-full mx-auto flex justify-between items-center gap-4">
         <div className="flex-1">
-          <h1 className="text-xl sm:text-xl font-bold text-white">
-            Dashboard
+          <h1 className="text-[20px] sm:text-[20px] font-medium text-white font-montserrat">
+            Welcome to Clash Admin Dashboard
           </h1>
         </div>
 
@@ -17,11 +18,13 @@ export function DashboardHeader() {
 
 
           <Avatar className="h-9 w-9 sm:h-10 sm:w-10 ring-2 ring-[#00FF88]/40 ring-offset-2 ring-offset-[#181818] transition-transform hover:scale-105">
+          <Link href={"/dashboard/account"}>
             <AvatarImage
-              src="https://blog.hootsuite.com/wp-content/uploads/2020/02/Image-copyright.png"
+              src="/dashboard-logo/Ellipse 6.svg"
               alt="User"
-              className="object-cover"
+              className="object-cover cursor-pointer"
             />
+          </Link>
             <AvatarFallback className="bg-linear-to-br from-[#00FF88]/20 to-[#00FF88]/10 text-white font-semibold text-sm sm:text-base">
               AD
             </AvatarFallback>
